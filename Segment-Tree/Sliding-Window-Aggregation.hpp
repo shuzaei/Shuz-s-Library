028bdbe95b180ca151e5e5de11f7223386f0380c
Shuzaei <shuzaei@gmail.com>
2019-12-01 15:41:26 +0900

    SWA

---
+++ Segment-Tree/Sliding-Window-Aggregation.hpp
@@ -0,0 +1,50 @@
+#ifndef def
+#include "Shuzaei.hpp"
+#endif
+
+template <class T = ll> struct SWA {
+    using op = function<T(T, T)>;
+    vector<T> A, B;
+    deque<T> D;
+    op f;
+    SWA(op f) : f(f) {}
+    void flip() {
+        if (A.size() == 0) {
+            B.clear();
+            for (auto i = D.end() - 1; i >= D.begin(); i--) {
+                if (i == D.end() - 1) {
+                    A.push_back(*i);
+                } else {
+                    A.push_back(f(A.back(), *i));
+                }
+            }
+        }
+    }
+    T operator()() {
+        flip();
+        if (A.size() == 0) {
+            abort();
+        } else if (B.size() == 0) {
+            return A.back();
+        } else {
+            return f(A.back(), B.back());
+        }
+    }
+    void push(T x) {
+        D.push_back(x);
+        if (B.size() == 0) {
+            B.push_back(x);
+        } else {
+            B.push_back(f(B.back(), x));
+        }
+    }
+    void pop() {
+        flip();
+        if (A.size() == 0) {
+            abort();
+        } else {
+            A.pop_back();
+            D.pop_front();
+        }
+    }
+};
