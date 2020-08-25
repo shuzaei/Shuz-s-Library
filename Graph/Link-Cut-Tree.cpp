e0941821356f224ead57222d430daa6d1db55530
Shuzaei <shuzaei@gmail.com>
2020-08-25 14:55:20 +0900

    　

--- Graph/Link-Cut-Tree.cpp
+++ Graph/Link-Cut-Tree.cpp
@@ -1,6 +1,8 @@
 struct link_cut_tree {
     struct node {
         node *pp, *lp, *rp;
+        ll idx, data;
+        node(ll i = 0, ll val = 0) : pp(nullptr), lp(nullptr), rp(nullptr), idx(i), data(val) {}
         bool is_root() { return pp == nullptr or (pp->lp != this and pp->rp != this); }
         void rotr() {
             node *q = pp, *r = q->pp;
@@ -29,7 +31,7 @@ struct link_cut_tree {
                     if (r->lp == q) {
                         q->lp == this ? q->rotr() : rotl(), rotr();
                     } else {
-                        q->lp == this ? q->rotl() : rotr(), rotl();
+                        q->rp == this ? q->rotl() : rotr(), rotl();
                     }
                 }
             }
@@ -48,4 +50,13 @@ struct link_cut_tree {
     }
     void cut(node *u, node *v) { expose(u), u->lp == v ? cut(u) : cut(v); }
     void link(node *c, node *p) { expose(c), expose(p), c->pp = p, p->rp = c; }
-};
+    node *get_root(node *x) {
+        expose(x);
+        while (x->lp != nullptr) x = x->lp;
+        return x;
+    }
+    node *lca(node *u, node *v) {
+        return get_root(u) == get_root(v) ? (expose(u), expose(v)) : nullptr;
+    }
+    node *make_node(ll idx) { return new node(idx); }
+} lct;
