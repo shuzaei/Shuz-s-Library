1f17c9bc572f9fee1e8e39fab5e32f3d2c4944eb
Shuz* <42332130+shuzaei@users.noreply.github.com>
2024-05-20 16:27:49 +0900

    Fix SWAG

--- Range/Sliding-Window-Aggregation.cpp
+++ Range/Sliding-Window-Aggregation.cpp
@@ -1,47 +1,72 @@
-template <class T = ll> struct SWA {
+template <class T = int64_t>
+struct SWA
+{
     using op = function<T(T, T)>;
     vector<T> A, B;
     deque<T> D;
     op f;
     SWA(op f) : f(f) {}
-    void flip() {
-        if (A.size() == 0) {
+    void flip()
+    {
+        if (A.size() == 0)
+        {
             B.clear();
-            for (auto i = D.end() - 1; i >= D.begin(); i--) {
-                if (i == D.end() - 1) {
-                    A.push_back(*i);
-                } else {
-                    A.push_back(f(A.back(), *i));
+            for (auto i = D.end(); i != D.begin(); i--)
+            {
+
+                if (i == D.end())
+                {
+                    A.push_back(*(i - 1));
+                }
+                else
+                {
+                    A.push_back(f(A.back(), *(i - 1)));
                 }
             }
         }
     }
-    T operator()() {
+    T operator()()
+    {
         flip();
-        if (A.size() == 0) {
+        if (A.size() == 0)
+        {
             raise(SIGSEGV);
-        } else if (B.size() == 0) {
+        }
+        else if (B.size() == 0)
+        {
             return A.back();
-        } else {
+        }
+        else
+        {
             return f(A.back(), B.back());
         }
+
+        return 0;
     }
-    void push(T x) {
+    void push(T x)
+    {
         D.push_back(x);
-        if (B.size() == 0) {
+        if (B.size() == 0)
+        {
             B.push_back(x);
-        } else {
+        }
+        else
+        {
             B.push_back(f(B.back(), x));
         }
     }
-    void pop() {
+    void pop()
+    {
         flip();
-        if (A.size() == 0) {
+        if (A.size() == 0)
+        {
             raise(SIGSEGV);
-        } else {
+        }
+        else
+        {
             A.pop_back();
             D.pop_front();
         }
     }
-    ll size() { return D.size(); }
+    int64_t size() { return D.size(); }
 };
